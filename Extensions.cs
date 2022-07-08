a6e57602803291a6d7a9cc332e5a7ed8e820532d
cscaliaSpiky <cscaliaspiky@gmail.com>
2022-07-08 11:49:40 -0300

    Lose Win Popup - List Get Random Extension

---
+++ Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+namespace PokeSpiky
+{
+    public static class Extensions
+    {
+        public static T GetRandom<T>(this IEnumerable<T> list)
+        {
+            int randomValue = Random.Range(0, list.Count());
+            return list.ElementAt(randomValue);
+        }
+    }
+}
