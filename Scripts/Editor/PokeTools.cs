838406096f5dd5f379c103ef2672aff6cb78e03b
Cristian Scalia <cristiansk8x@gmail.com>
2023-02-13 15:35:24 -0300

    Basic Package Update And Initialization

--- Scripts/Editor/PokeTools.cs
+++ Scripts/Editor/PokeTools.cs
@@ -1,21 +1,43 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
+[InitializeOnLoad]
 public class PokeTools
 {
-    [MenuItem("Spiky Tools/Poke/Initial Setup 1")]
+    static PokeTools()
+    {
+        AssetDatabase.importPackageCompleted -= OnImportPackageCompleted;
+        AssetDatabase.importPackageCompleted += OnImportPackageCompleted;
+    }
+    
+    [MenuItem("Spiky Tools/Poke/Initial Setup")]
     private static void StartInitialSetupTool1()
     {
         PlayerSettings.applicationIdentifier = "com.spiky."+ Application.productName.ToLower().Replace(" ","");
         AssetDatabase.ImportPackage("Assets/PokeCore/Packages/BasicScenesAndScripts.unitypackage", false);
         AssetDatabase.SaveAssets();
     }
-    
-    [MenuItem("Spiky Tools/Poke/Initial Setup 2")]
-    private static void StartInitialSetupTool2()
+
+    private static void OnImportPackageCompleted(string obj)
     {
-        string gameFolderName = "_" + Application.productName.Replace(" ", string.Empty);
-        AssetDatabase.MoveAsset("Assets/_GameName", "Assets/"+ gameFolderName);
-        AssetDatabase.SaveAssets();
+        if (obj.Equals("BasicScenesAndScripts"))
+        {
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            AssetDatabase.MoveAsset("Assets/_GameName", "Assets/"+ GameFolderName);
+        }
     }
+    
+    [MenuItem("Spiky Tools/Poke/Update Init Package")]
+    private static void UpdateInitPackage()
+    {
+        AssetDatabase.MoveAsset("Assets/"+ GameFolderName, "Assets/_GameName");
+        string[] assets = { "Assets/_GameName" };
+        AssetDatabase.ExportPackage(assets, "Assets/PokeCore/Packages/BasicScenesAndScripts.unitypackage", ExportPackageOptions.Recurse);
+        AssetDatabase.MoveAsset("Assets/_GameName", "Assets/"+ GameFolderName);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+        
+    static string  GameFolderName => "_" + Application.productName.Replace(" ", string.Empty);
 }
