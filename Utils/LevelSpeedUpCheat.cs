5bc1bb5b5ca7408ce98b1b3964edce26554648a6
Cristian Scalia <cristiansk8x@gmail.com>
2023-02-13 11:58:42 -0300

    Basic Cheats

---
+++ Utils/LevelSpeedUpCheat.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace PokeSpiky
+{
+    public class LevelSpeedUpCheat : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (!Application.isEditor)
+                return;
+
+            if (Input.GetKeyDown(KeyCode.Q))
+                ModifyLevel(-1);
+            if (Input.GetKeyDown(KeyCode.E))
+                ModifyLevel(1);
+            if (Input.GetKeyDown(KeyCode.R))
+                DefaultGameEventSystemManager.Instance.GameEventSystem.Dispatch(new ForceReloadLevelEvent());
+            if (Input.GetKeyDown(KeyCode.Space))
+                Time.timeScale = 3;
+            else if (Input.GetKeyUp(KeyCode.Space))
+                Time.timeScale = 1;
+        }
+        
+        private static void ModifyLevel(int levelAdd)
+        {
+            DefaultGameEventSystemManager.Instance.GameEventSystem.Dispatch(new ForceOverrideUserLevelEvent(levelAdd));
+            UpdateService.Instance.CancelAction(ReloadScene);
+            UpdateService.Instance.ExecuteActionAfterSeconds(.25f, ReloadScene);
+        }
+
+        private static void ReloadScene()
+        {
+            DefaultGameEventSystemManager.Instance.GameEventSystem.Dispatch(new ForceReloadLevelEvent());
+        }
+    }
+}
